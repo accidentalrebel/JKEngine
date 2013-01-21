03736d23206030c313f79a9a906c9b050745c2d5
Karlo Licudine <karlo@accidentalrebel.com>
2013-01-21 14:21:26 +0800

    Added comments to JK2DArray

--- JK2DArray.hx
+++ JK2DArray.hx
@@ -12,43 +12,56 @@ class JK2DArray
 	
     public function new(width: Int, height: Int) 
     { 
-		array = new Array<Dynamic>();
-		 
+		array = new Array<Dynamic>();		 
 		pitch = width;
     } 
 
-    public function get(width: Int, height: Int) 
+	/**
+	 * Get's the content of the array at the x and y positions
+	 * @param	xPos	the x position
+	 * @param	yPos	the y position
+	 */
+    public function get(xPos: Int, yPos: Int) 
     {
-		return array[height * pitch + width]; 
+		return array[xPos * pitch + yPos]; 
     }
-	
-	public function set(toPut : Dynamic, width: Int, height: Int) 
+		
+	/**
+	 * Places a value "toPut" at the specified x and y positions
+	 * @param	toPut	The object to be placed at position
+	 * @param	xPos	The x position
+	 * @param	yPos	The y position
+	 */
+	public function set(toPut : Dynamic, xPos: Int, yPos: Int) 
     { 
-		array[height * pitch + width] = toPut;
+		array[yPos * pitch + xPos] = toPut;
     }
 	
+	/**
+	 * Displays the content of the whole array using trace()
+	 */
 	public function traceAllContent()
 	{
 		var xElementCount : Int = 1;
 		var i : Int = 0;
-		var toDisplay : String = "\n";
-		for ( element in array )
+		var toDisplay : String = "\n";			// We set up the toDisplay string
+		for ( element in array )				// We loop through each element
 		{
-			if ( element == null )
+			if ( element == null )				// If the element is empty, "N" is displayed
 				toDisplay += "N";
 			else
-				toDisplay += "1";
+				toDisplay += "1";				// If element is not empty, "1" is displayed
 			
-			if ( xElementCount >= pitch )
+			if ( xElementCount >= pitch )		// This checks whether we should go to the next line
 			{
-				xElementCount = 0;
-				toDisplay += "\n";
+				xElementCount = 0;				// We reset the xElement count
+				toDisplay += "\n";				// We then add a new line
 			}
 			
 			xElementCount ++;
 			i++;
 		}
 		
-		trace(toDisplay);
+		trace(toDisplay);						// We display the generated string
 	}	
 }
