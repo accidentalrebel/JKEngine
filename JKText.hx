8acebe543b960acb2d596bf8583c938161b1a611
Karlo Licudine <karlo@accidentalrebel.com>
2013-01-21 16:38:57 +0800

    Added comments to JKText

--- JKText.hx
+++ JKText.hx
@@ -1,10 +1,11 @@
 package jkEngine;
-import nme.text.TextField;
+
 import nme.display.DisplayObjectContainer;
-import nme.Lib;
+import nme.text.TextFormatAlign;
 import nme.text.TextFieldType;
 import nme.text.TextFormat;
-import nme.text.TextFormatAlign;
+import nme.text.TextField;
+import nme.Lib;
 
 /**
  * ...
@@ -14,7 +15,7 @@ import nme.text.TextFormatAlign;
 class JKText extends TextField
 {
 	var textFormat : TextFormat;
-	
+		
 	public function new( xPos : Float = 0, yPos : Float = 0, ?theWidth : Float
 		, ?theHeight : Float, ?theText : String, theTextColor : Int = 0x000000
 		, fontSize : Int = 10, alignment : TextFormatAlign = null, fontType : String, ?layer : DisplayObjectContainer ) 
@@ -33,32 +34,36 @@ class JKText extends TextField
 			height = theHeight;
 			
 		// Set the text format		
-		multiline = true;
-		wordWrap = true;
+		multiline = true;									// Enable multiline
+		wordWrap = true;									// Enable wordwrap
 		textFormat = new TextFormat();		
-		textColor = theTextColor;	
+		textColor = theTextColor;							// We set the textColor
 		
 		if ( alignment != null )		
-			textFormat.align = alignment;		
+			textFormat.align = alignment;					// We set the alignment
 			
-		textFormat.size = fontSize;	
-		textFormat.font = fontType;
+		textFormat.size = fontSize;							// We set the font size
+		textFormat.font = fontType;							// We set the font type
 		
 		// Set the text
 		if ( theText != null )
-			setText(theText);
+			setText(theText);								// We set the actual text
 			
 		// Setting of layer
 		if ( layer == null )
 			layer = Lib.stage;			
 				
-		layer.addChild(this);				// Add to layer
-		setTextFormat(textFormat);			// Apply text format
+		layer.addChild(this);								// Add to layer
+		setTextFormat(textFormat);							// Apply text format
 	}
 	
+	/**
+	 * Sets the text to be displayed. Use this instead of accessing text property directly.
+	 * @param	theText		The string to display
+	 */
 	public function setText(theText : String )
 	{		
-		text = theText;
-		setTextFormat(textFormat);
+		text = theText;										// We set the actual text
+		setTextFormat(textFormat);							// We reapply the text format
 	}
 }
