502147d2d989fb085601b490bcb3a3c74bf2af9e
Karlo Licudine <karlo@accidentalrebel.com>
2013-01-22 11:00:21 +0800

    position of moused is saved to position:JKPoint

--- input/JKMouse.hx
+++ input/JKMouse.hx
@@ -13,22 +13,38 @@ class JKMouse extends JKObject
 {
 	var mousePressed : Int ;
 	var isMousePressed : Bool = false;
+	public var position : JKPoint;
 	
 	public function new() 
 	{
 		super();
 		
-		Lib.stage.addEventListener(MouseEvent.MOUSE_DOWN, onMouseDown);			// WE listen to a KEY_UP event
+		position = new JKPoint();
+		Lib.stage.addEventListener(MouseEvent.MOUSE_DOWN, onMouseDown);			
+		Lib.stage.addEventListener(MouseEvent.MOUSE_MOVE, onMouseMove);			
 	}
 	
 	function onMouseDown(e : MouseEvent)
-	{
+	{		
 		isMousePressed = true;
+	}
+	
+	function onMouseMove(e : MouseEvent )
+	{
+		position.x = e.stageX;
+		position.y = e.stageY;
 	}
 	
 	public function justPressed() : Bool
 	{
 		return isMousePressed;
+	}
+	
+	override private function update():Dynamic 
+	{
+		super.update();
+		
+		
 	}
 	
 	override private function lateUpdate():Dynamic 
