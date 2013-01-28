5a8f294ddee947353838d4c4de875512f809ea4a
Karlo <Karlo@localhost>
2013-01-28 10:41:45 +0800

    Added a collide function that checks for colllision between two objects

--- JKUtils.hx
+++ JKUtils.hx
@@ -1,10 +1,26 @@
 package jkEngine;
-
+import nme.geom.Rectangle;
+import nme.Lib;
 class JKUtils 
 {	
 	public function new() 
 	{
 		
 	}	
+	
+	/**
+	 * Checks if there is a collision
+	 */
+	public static function collide(?object1 : JKObject, ?group1 : Array<JKObject>, ?object2 : JKObject, ?group2 : Array<JKObject>) : Bool
+	{
+		var object1Bounds : Rectangle = object1.getBounds(Lib.stage);
+		if ( object1.hitTestObject(object2))
+		{
+			trace("collision!");
+			return true;
+		}
+		
+		return false;
+	}
 }
 
