488518f2c7d9a54b24e4f5490f64fa3a7d3b92ee
Karlo <Karlo@localhost>
2013-01-29 11:16:48 +0800

    LoadMap is now placed in JKTileMap and it can now accept thae class to instantiate

--- JKTileMap.hx
+++ JKTileMap.hx
@@ -18,6 +18,11 @@ class JKTileMap extends JK2DArray
 		//populateTileMap(TheLayer);
 	}
 	
+	/**
+	 * Populates the whole map with the given class
+	 * @param	myClass			The class to instantiate with	
+	 * @param	parameters		The parameters that will be set for the class
+	 */
 	function populateTileMap<T>(myClass : Class<T>, parameters : Array<Dynamic>) : Void
 	{
 		for ( i in 0...arrayWidth )
@@ -31,14 +36,25 @@ class JKTileMap extends JK2DArray
 			}
 		}
 	}
-	
-	public function loadMap(MapData: String)
+
+	/**
+	 * Instantiates the tile class according to the given map data and places them in their respective positions
+	 * @param	MapData			The map data
+	 * @param	myClass			The class to insntiate
+	 * @param	parameters		The parameters for the class. Note: Class must have a constructor for XCoord and YCoord.
+	 */
+	public function loadMap<T>(MapData: String, myClass : Class<T>, parameters : Array<Dynamic>)
 	{
+		var valueIndex : Int = parameters.length;
 		for ( j in 0...arrayHeight )
 		{
 			for ( i in 0...arrayWidth )
 			{	
-				set(new Tile(i, j, 40, 40, "img/mapTiles.png", theLayer, Std.parseInt(MapData.charAt(( j * arrayWidth ) + i ))), i, j);
+				parameters[0] = i;												// We override the value of xCoordinate
+				parameters[1] = j;												// We override the value of yCoordinate
+				parameters[valueIndex] = Std.parseInt(MapData.charAt(( j * arrayWidth ) + i ));		// We set the tile value according to the map data
+				var instance = Type.createInstance(myClass, parameters);		// We create the class instance
+				set(instance, i, j);											// We set the instance inside our array
 			}
 		}
 	}
